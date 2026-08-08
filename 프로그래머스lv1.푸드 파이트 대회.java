8b07c8e1d1f056cd509b8931c54e411f7f4bfe26
형민 <150722125+hyeongmin02@users.noreply.github.com>
2026-08-08 18:30:31 +0900

    Create 프로그래머스lv1.푸드 파이트 대회.java

---
+++ 프로그래머스lv1.푸드 파이트 대회.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        for(int i=1;i<food.length;i++){
+            for(int j =0;j<food[i]/2;j++){
+                answer+=i;
+            }
+        }
+        String reversed = new StringBuilder(answer).reverse().toString();
+        answer+=0+reversed;
+        return answer;
+    }
+}
